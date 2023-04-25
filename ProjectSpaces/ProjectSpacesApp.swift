d3aafe669e84ff7bf86594cb7695b3a6d0a2a2ff
Oleksii Perov <oleksii.perov@gmail.com>
2023-04-25 23:34:54 +0300

    Initial Commit

---
+++ ProjectSpaces/ProjectSpacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSpacesApp.swift
+//  ProjectSpaces
+//
+//  Created by Oleksii Perov on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSpacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
